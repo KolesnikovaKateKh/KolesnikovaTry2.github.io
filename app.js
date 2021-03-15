cf14c9070be6a2ac455c2d4a06f94a9b123cd0fa
Kate <Kant1platon@mail.ru>
2021-03-15 16:51:06 +0200

    added sitebar

--- app.js
+++ app.js
@@ -169,16 +169,200 @@
 // console.log(`Цена товара со скидкой ${finishPrice3} ${current}`)
 
 
-function finishPrice () {
+// function finishPrice () {
 
-    let  current= "грн";
-    let sale = 20;
-    let price = prompt ("price?");
-    let discountPrice = price-(price*sale/100);
+//     let  current= "грн";
+//     let sale = 20;
+//     let price = prompt ("price?");
+//     let discountPrice = price-(price*sale/100);
 
-    console.log(`Цена товара со скидкой ${discountPrice} ${current}`)
+//     console.log(`Цена товара со скидкой ${discountPrice} ${current}`)
+// }
+
+// finishPrice ()
+// finishPrice ()
+// finishPrice ()
+
+
+  // ....................................Домашка № 14
+
+// ....................................Зачада №1
+
+// function calcNumber () {
+
+//     let a = prompt ("a?");
+//     let b = prompt ("b?");
+
+//     return (a<b) ? b:a 
+    
+// }
+
+// console.log(calcNumber ())
+
+// ....................................Зачада №2
+               
+//     function yearsUntilRetirement(year,userName) {
+            
+//     userName = prompt ("Укажите ваше имя");
+//     let age = prompt ("Укажите год рождения");
+
+//             function calcAge()  {
+
+//         let year= 2021-age;
+
+//         console.log(`u age is ${year}`) 
+                    
+//         let retirementAge =60;
+        
+//         let retirement = retirementAge-year;
+        
+//            if (year>=60) {
+//                 alert (`${userName} вже на пенсіонер`)
+//             } else {
+//                 alert (`${userName} вийде на пенсію через ${retirement} років`)
+//                 }
+//             }
+//             calcAge() 
+//         }
+
+//    yearsUntilRetirement()
+    
+
+// ....................................Зачада №3
+
+// function sale() {
+
+//     let sum = prompt ("Введите сумму покупок в магазине")
+
+//     if(sum<1000) {
+//         saleCost = 0.03
+//     } else if (sum > 1000 && sum < 5000 ) {
+//         saleCost = 0.05
+//     } else if(sum > 5000 ){
+//         saleCost = 0.1
+//     }
+
+//     let finishPrice = sum - (sum*saleCost)
+    
+//     console.log(`Ваша скидка составляет ${saleCost*100}%, a сумма покупок со скидкой ${finishPrice} грн`) 
+// }
+
+// sale()
+
+// ....................................Зачада №4
+
+
+
+// function calcSum() {
+    
+//     let price = prompt ("price?")
+//     let quantity = prompt ("quantity?")
+
+//     let price2 = prompt ("price2?")
+//     let quantity2 = prompt ("quantity2?")
+
+//     let allSum = (price*quantity) + (price2*quantity2);
+
+//     console.log(`Общая сумма ваших покупопк - ${allSum} грн`) 
+// }
+
+// calcSum()
+
+
+//........................... занятие №15
+
+//     let  current= "грн";
+//     let discount = 20;
+
+
+//     function calcDiscount(productNumber) {
+
+    
+//     let productPrice = prompt (`price ${productNumber} product?`);
+//     let productPriceWithDiscount = productPrice-(productPrice*discount/100)
+
+//     console.log(`Цена ${productNumber} товара со скидкой ${productPriceWithDiscount} ${current}`)
+// }
+
+//     calcDiscount (1)
+//     calcDiscount (2)
+//     calcDiscount (3)
+
+// ///////////////////////ВТОРОЕ РЕШЕНИЕ
+
+
+//     let  current= "грн";
+//     let discount = 20;
+//     let productNumber =1;
+
+
+//     function calcDiscount() {
+
+    
+//     let productPrice = prompt (`price product?`);
+//     let productPriceWithDiscount = productPrice-(productPrice*discount/100)
+
+//     console.log(`Цена ${productNumber++} товара со скидкой ${productPriceWithDiscount} ${current}`)
+// }
+
+//     calcDiscount ()
+//     calcDiscount ()
+//     calcDiscount ()
+
+
+// function test() {
+//     return 3 + 4
+// }
+
+// console.log (test())
+
+
+// let userName = "Ivan";
+
+// function calcAge (age) {
+//     return 2020-age
+
+// }
+
+// console.log (calcAge(1990))
+
+// function yearsUntilRetirement (year,userName) {
+
+//     let age = calcAge(year);
+//     let retirement = 60 - age;
+
+//     if(retirement > 0) {
+//         return (`${userName} выйдет на персию через ${retirement} лет`)
+//     } else  {
+//         return (`${userName} уже пенсионер`)
+//     }
+// }
+
+// console.log (yearsUntilRetirement(1985, "Ivan"))
+
+
+////////////////////// ОБЬЕКТЫ
+
+// let obj = {
+
+//   key:value,
+
+// }
+
+let user = {
+
+  userName: "Kate",
+  surname: "Kolesnikova",
+  age: "17",
+  phoneNumber: "87868678686"
 }
 
-finishPrice ()
-finishPrice ()
-finishPrice ()
+user.city ="Kyiv"
+
+user.city ="Lviv"
+
+delete user.city
+
+console.log(user)
+console.log(user.userName)
+console.log(user['userName'])
